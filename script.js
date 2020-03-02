61c5b3a5cb02bf28f88c100d086dc08b5bf8d60d
kaizen-minded <melvin3ezell@gmail.com>
2020-03-02 13:01:12 -0500

    add script for animation

---
+++ script.js
@@ -0,0 +1,55 @@
+const app = () => {
+  const song = document.querySelector(".song");
+  const play = document.querySelector(".play");
+  const outline = document.querySelector(".moving-outline circle");
+  const video = document.querySelector(".vid-container video");
+
+  //Sounds
+  const sounds = document.querySelectorAll(".sound-picker button");
+  //Time Display
+  const timeDisplay = document.querySelector(".time-display");
+  //Get the length of the outline
+  const outlineLength = outline.getTotalLength();
+  //Duration
+  let fakeDuration = 600;
+
+  outline.style.strokeDasharray = outlineLength;
+  outline.style.strokeDashoffset = outlineLength;
+
+  //play sound
+  play.addEventListener('click', () => {
+    console.log('Play')
+    checkPlaying(song)
+  })
+
+
+  //Create a function specific to stop and play sounds
+  const checkPlaying = song => {
+    if(song.paused){
+      song.play();
+      video.play();
+      play.src = './svg/pause.svg';
+    } else {
+      song.pause();
+      video.pause();
+      play.src= "./svg/play.svg"
+    }
+  };
+
+  //Animate the circle
+  song.ontimeupdate = () => {
+    let currentTime = song.currentTime;
+    let elapsed = fakeDuration - currentTime;
+    let seconds = Math.floor(elapsed % 60);
+    let minutes = Math.floor(elapsed / 60);
+
+    //Animate the circle
+    let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+    outline.style.strokeDashoffset = progress;
+    //Animate the text
+    timeDisplay.textContent = `${minutes}:${seconds}`;
+  }
+  
+}
+
+app();
