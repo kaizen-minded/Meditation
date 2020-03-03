63aa7e727d9b669e583f336e70c9ba11bbc002dc
kaizen-minded <melvin3ezell@gmail.com>
2020-03-03 12:49:03 -0500

    complete functionality of buttons and timer display

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const app = () => {
   const sounds = document.querySelectorAll(".sound-picker button");
   //Time Display
   const timeDisplay = document.querySelector(".time-display");
+  const timeSelect = document.querySelectorAll(".time-select button");
   //Get the length of the outline
   const outlineLength = outline.getTotalLength();
   //Duration
@@ -16,12 +17,27 @@ const app = () => {
   outline.style.strokeDasharray = outlineLength;
   outline.style.strokeDashoffset = outlineLength;
 
+  //Pick different sounds
+  sounds.forEach(sound => {
+    sound.addEventListener('click', function() {
+      song.src = this.getAttribute('data-sound');
+      video.src = this.getAttribute('data-video');
+      checkPlaying(song);
+    })
+  })
   //play sound
   play.addEventListener('click', () => {
     console.log('Play')
     checkPlaying(song)
   })
 
+  //Select sound
+  timeSelect.forEach(option => {
+    option.addEventListener('click', function() {
+      fakeDuration = this.getAttribute('data-time');
+      timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(fakeDuration % 60)}`
+    });
+  })
 
   //Create a function specific to stop and play sounds
   const checkPlaying = song => {
@@ -48,6 +64,13 @@ const app = () => {
     outline.style.strokeDashoffset = progress;
     //Animate the text
     timeDisplay.textContent = `${minutes}:${seconds}`;
+    
+    if(currentTime >= fakeDuration) {
+      song.pause();
+      song.currentTime = 0;
+      play.src = './svg/play.svg';
+      video.pause();
+    }
   }
   
 }
